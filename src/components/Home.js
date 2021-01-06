602e1256e488bc364831f21cd8b9ae941b333a09
nilabhanand <nilabhanand10@gmail.com>
2021-01-05 20:32:22 -0500

    working on user profiles

--- src/components/Home.js
+++ src/components/Home.js
@@ -17,13 +17,16 @@ import Charts from "./Charts";
 import Typography from "@material-ui/core/Typography";
 import firebase from "firebase/app";
 import "firebase/auth";
+import Toolbar from '@material-ui/core/Toolbar';
 
 class Home extends Component {
     constructor(props) {
         super(props);
         this.logout = this.logout.bind(this);
+        this.settings = this.settings.bind(this);
         this.state = {
-            activeTab: 0
+            activeTab: 0,
+            showing: true
         }
     }
 
@@ -38,21 +41,29 @@ class Home extends Component {
         });
     }
 
+    settings = e => {
+        console.log("settings clicked");
+    }
+
     handleChange = (event, value) => {
         this.setState({ activeTab: value });
     };
 
     render() {
-
+        const { showing } = this.state;
         return (
             <div>
                 <AppBar position="static" color="#0e1417" className="appbar">
-                    <Tabs className="main-tabs" value={this.state.activeTab} onChange={this.handleChange} centered>
+                    <Tabs className="main-tabs" value={this.state.activeTab} onChange={this.handleChange} variant="fullWidth">
                         <Tab label="Terminal" style={{ color: "#fff", fontSize: "12px" }}/>
                         <Tab label="Markets" style={{ color: "#fff", fontSize: "12px" }}/>
                         <Tab label="Stats" style={{ color: "#fff", fontSize: "12px" }}/>
                         <Tab label="Community" style={{ color: "#fff", fontSize: "12px" }}/>
-                        <Button onClick={this.logout} style={{ color: "#fff", fontSize: "12px" }}>Logout</Button>
+                        <Toolbar>
+                            <Button onClick={this.logout} style={{ color: "#fff", fontSize: "12px"}}>Logout</Button>
+                            <Button onClick={() => this.setState({ showing: !showing})} style={{ color: "#fff", fontSize: "12px"}}>Settings</Button>
+                            <div style={{ display: (showing ? 'block' : 'none') }}>SETTINGS DIV</div>
+                        </Toolbar>
                     </Tabs>
                     {this.state.activeTab === 0 && (
                         <Forum />
--- src/components/Login.js
+++ src/components/Login.js
@@ -10,13 +10,14 @@ import "firebase/auth";
 class Login extends Component {
     constructor(props) {
         super(props);
+        this.showSignup = this.showSignup.bind(this);
         this.login = this.login.bind(this);
         this.signup = this.signup.bind(this);
         this.handleChange = this.handleChange.bind(this);
         this.state = {
             email: '',
             password: '',
-            firstName: '',
+            fullName: '',
             lastName: ''
         }
     }
@@ -27,14 +28,35 @@ class Login extends Component {
         }).catch((error) => {
             console.log(error);
         });
+        var user = firebase.auth().currentUser;
+        if (user != null) {
+            user.providerData.forEach(function (profile) {
+              console.log("  Name: " + profile.displayName);
+              console.log("  Email: " + profile.email);
+              console.log("  Photo URL: " + profile.photoURL);
+            });
+        }
     }
 
+    showSignup(e) {
+        e.preventDefault();
+        document.getElementById('login-form').style.visibility = "hidden";
+    }
     signup(e) {
         e.preventDefault();
         firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.password)
         .catch((error) => {
             console.log(error);
         })
+        var user = firebase.auth().currentUser;
+        user.updateProfile({
+            displayName: this.state.fullName
+        }).then(function() {
+            // Update successful.
+            console.log(user.displayName)
+        }).catch(function(error) {
+            // An error happened.
+        });
     }
 
     handleChange(e) {
@@ -44,7 +66,28 @@ class Login extends Component {
         return (
             <div className="login-frame">
                 <div className="right-frame">
-                <form className="login-form">
+                <form id="signup-form">
+                    <div className="login-form-group">
+                        <div className="login-form-input">
+                            <Typography for="exampleInputEmail1" style={{ textAlign: "left", color: "#fff", fontSize: "18px", marginBottom: "10px" }}>FullName</Typography>
+                            <input className="login-textfield" value={this.state.firstName} onChange={this.handleChange} type="name" name="firstname" id="exampleInputEmail1" placeholder="Full Name" />
+                            <div className="space"></div>
+
+                            <Typography for="exampleInputEmail1" style={{ textAlign: "left", color: "#fff", fontSize: "18px", marginBottom: "10px" }}>Email Address</Typography>
+                            <input className="login-textfield" value={this.state.email} onChange={this.handleChange} type="email" name="email" id="exampleInputEmail1" placeholder="Enter Email" />
+                            <div className="space"></div>
+                        
+                            <Typography for="exampleInputPassword1" style={{ textAlign: "left", color: "#fff", fontSize: "18px", marginBottom: "10px"}}>Password</Typography>
+                            <input className="login-textfield" value={this.state.password} onChange={this.handleChange} type="password" name="password" id="exampleInputPassword1" placeholder="Password" />
+                            <div className="space"></div>
+                        </div>
+                        <div className="login-form-button">
+                            <div className="space"></div>
+                            <button className="auth-buttons" onClick={this.signup}>Create Account</button>
+                        </div>
+                    </div>
+                </form>
+                <form id="login-form">
                     <div className="login-form-group">
                         <div className="login-form-input">
                             <Typography for="exampleInputEmail1" style={{ textAlign: "left", color: "#fff", fontSize: "18px", marginBottom: "10px" }}>Email Address</Typography>
@@ -58,7 +101,7 @@ class Login extends Component {
                         <div className="login-form-button">
                             <button className="auth-buttons" type="submit" onClick={this.login}>Login</button>
                             <div className="space"></div>
-                            <button className="auth-buttons" onClick={this.signup}>Sign Up</button>
+                            <button className="auth-buttons" onClick={this.showSignup}>Sign Up</button>
                         </div>
                     </div>
                 </form>
